a89ef4a6e92776db352217b04d04263e01bb07dc
Vishnu P S <vishnu13three@gmail.com>
2026-01-31 18:07:16 +0530

    refactor(routes): update auth routes to use dependency injection

--- src/presentation/routes/auth.routes.ts
+++ src/presentation/routes/auth.routes.ts
@@ -1,29 +1,28 @@
 import { Router } from "express";
-import { AuthController } from "../controllers/auth.controller";
+
 import { jwtAuthMiddleware } from "../../infrastructure/security/jwt-auth.middleware";
-import { rootCertificates } from "node:tls";
 import { container } from "../../infrastructure/di/container";
 import { TYPES } from "../../shared/di/types";
+import { AuthController } from "../controllers/auth.controller";
 
-const authController  = container.get<AuthController>(
+const router = Router();
+
+const authController = container.get<AuthController>(
   TYPES.AuthController
 );
 
-const router = Router();
-// router.post ('/register', AuthController.register);
 router.post('/login', authController.login.bind(authController));
 
-// router.get('/me', jwtAuthMiddleware,(req,res) => {
-//     const user = (req as any). user;
+router.post(
+  "/request-otp",
+  authController.requestSignupOtp.bind(authController)
+);
 
-//     return res.status(200).json({
-//         message: 'Authenticated',
-//         user,
-//     });
-// })
-router.post("/request-otp", authController.requestSignupOtp.bind(authController))
+router.post(
+  "/signup",
+  authController.signup.bind(authController)
+);
 
-router.post("/signup", authController.signup.bind(authController));
 router.post(
   "/forgot-password/request-otp",
   authController.requestPasswordResetOtp.bind(authController)
@@ -40,19 +39,19 @@ router.post(
 );
 
 router.get(
-  '/me', 
+  '/me',
   jwtAuthMiddleware,
   authController.me.bind(authController)
-)
+);
+
 router.post(
   '/refresh',
   authController.refresh.bind(authController)
-)
+);
 
 router.post(
   '/logout',
   authController.logout.bind(authController)
-)
-
+);
 
 export default router;
