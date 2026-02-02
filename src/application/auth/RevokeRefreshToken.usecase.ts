ce7b668f55ff09fd6b3b8d5dec32910819c10ed8
Vishnu P S <vishnu13three@gmail.com>
2026-02-02 23:53:27 +0530

    fix(auth): hash refresh token inside RevokeRefreshTokenUseCase

--- src/application/auth/RevokeRefreshToken.usecase.ts
+++ src/application/auth/RevokeRefreshToken.usecase.ts
@@ -1,15 +1,20 @@
 import { inject, injectable } from "inversify";
 import { TYPES } from "../../shared/di/types";
 import { RefreshTokenRepository } from "../../domain/auth/RefreshTokenRepository";
+import { TokenServicePort } from "../ports/TokenServicePort";
 
 @injectable()
 export class RevokeRefreshTokenUseCase {
     constructor (
         @inject(TYPES.RefreshTokenRepositoryPort)
-        private readonly refreshTokenRepository: RefreshTokenRepository
+        private readonly refreshTokenRepository: RefreshTokenRepository,
+        
+        @inject(TYPES.TokenServicePort)
+        private readonly tokenService: TokenServicePort,
     ) {}
 
-    async execute(tokenHash: string): Promise<void> {
+    async execute(plainToken: string): Promise<void> {
+        const tokenHash = this.tokenService.hashToken(plainToken)
         await this.refreshTokenRepository.revoke(tokenHash);
     }
 }
